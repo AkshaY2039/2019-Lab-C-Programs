c56eb35d3f1455509ee48c21fc0783b7d0218497
AkshaY2039 <ced15i031@iiitdm.ac.in>
2019-11-16 01:04:06 +0530

    Array Inside Union - Done

--- 42_Array_inside_Union.c
+++ 42_Array_inside_Union.c
@@ -4,7 +4,27 @@
 */
 #include <stdio.h>
 
+union two
+{
+	int num;
+	char arr[2];
+};
+
 int main ()
 {
+	union two test;
+
+	printf ("Enter the first character : ");
+	scanf ("%c", &test.arr[0]);
+	getchar ();
+	printf ("Enter the second character : ");
+	scanf ("%c", &test.arr[1]);
+	
+	printf ("Before Swap :\n\tNumber is %d\n\tFirst character is \'%c\'\n\tSecond character is \'%c\'\n", test.num, test.arr[0], test.arr[1]);
+	char temp =test.arr[0];
+	test.arr[0] = test.arr[1];
+	test.arr[1] = temp;
+	printf ("After Swap :\n\tNumber is %d\n\tFirst character is \'%c\'\n\tSecond character is \'%c\'\n", test.num, test.arr[0], test.arr[1]);
+
 	return 0;
 }
